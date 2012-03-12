30f45297234f4e5e3244a81eb96fdd65e0431a9a
Hyeongjin, Kim <1981_khj@naver.com>
2012-03-12 02:12:39 +0000

    "change"

--- public/js/webchat.js
+++ public/js/webchat.js
@@ -5,7 +5,7 @@
     //var socket = io.connect('http://p9chat.hjkim.c9.io');
     
     //for deploy
-	var socket = io.connect('http://p4chat.herokuapp.com/');
+	var socket = io.connect('http://p5chat.herokuapp.com/');
     var joined = false;
 
     // 서비스에 접속한 경우 자신의 대화명을 좌측 상단 아이콘 옆에 출력
